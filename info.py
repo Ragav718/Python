8d0b3a1aa9e6f4f40fdc9497aa6c1dbd07077836
Ragav718 <ragavan.m.d.718@gmail.com>
2025-01-21 12:05:22 +0530

    Indian captain info updated

--- info.py
+++ info.py
@@ -1,2 +1,2 @@
-a="Tomorrow, 1st T20 will be played between India and England at Eden gardens. "
+a="Tomorrow, 1st T20 will be played between India and England at Eden gardens. India team will be lead by Surya Kumar Yadav "
 print(a)
